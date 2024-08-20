273da5a34abe4b39b76e33b0ad954b7dcf37120f
NullAux <gverney@outlook.com>
2024-08-20 15:26:29 +0100

    Add first test for existing functionality. Moved seed database into its own macro to allow faster testing

--- __tests__/test.js
+++ __tests__/test.js
@@ -4,8 +4,16 @@ const request = require("supertest")
 //const db = require("./db-index.js")
 const app = require("../server_files/app.js")
 
-describe("Describe block", () => {
-    test("test one", () => {
-
+describe("GET /", () => {
+    test("returns a JSON", () => {
+        return request(app).get("/")
+        .expect(200)
+        .expect("Content-type","application/json; charset=utf-8")
+    })
+    test("Body contains expected message", () => {
+        return request(app).get("/")
+        .then((result) => {
+            expect(result.body).toEqual({"msg": "Welcome to the server!"})
+        })
     })
 })
